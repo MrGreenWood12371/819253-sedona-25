8c362b522dfc3f32512fd76fb07f0c9b82abdd71
MrGreenWood12371 <45576782+MrGreenWood12371@users.noreply.github.com>
2019-06-23 16:30:18 +0300

    Исправил ошибки

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 var popupButton = document.querySelector('.button-search');
 var popup = document.querySelector('.hotel-popup');
+popup.classList.add('shide');
 popupButton.addEventListener('click', function(evt){
   evt.preventDefault();
   if(popup.classList.contains('shide')){
